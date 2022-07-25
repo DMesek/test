cb29008f03f5e8c17cee7cf602e1e76245998793
dmesek <dominik.mesek@q.agency>
2022-07-25 15:30:22 +0200

    changes

--- step.sh
+++ step.sh
@@ -12,7 +12,6 @@ if [[ ! -z ${config_file_content} ]]; then
   echo "${config_file_content}" > default.conf
   sudo rm /etc/vpnc/default.conf
   sudo mv default.conf /etc/vpnc
-  sudo cat /etc/vpnc/default.conf
 fi
 
 set -- ${commandline_options}
