6912364ecb0146a82945d98425c18e4154d031c3
dmesek <dominik.mesek@q.agency>
2022-07-25 15:25:23 +0200

    more modifications

--- step.sh
+++ step.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 set -e
 
+git clone https://github.com/breiter/vpnc.git
+cd vpnc
+make
+sudo make install
+
+echo "Successfully installed vpnc."
+
 if [[ ! -z ${config_file_content} ]]; then
   echo "${config_file_content}" > default.conf
   sudo rm /etc/vpnc/default.conf
@@ -10,3 +17,5 @@ fi
 
 set -- ${commandline_options}
 sudo vpnc "$@"
+
+echo "Connected via VPNC."
