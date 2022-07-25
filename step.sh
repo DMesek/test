d73b9277642fa3c090b00d5c17ebdc357331f8dd
dmesek <dominik.mesek@q.agency>
2022-07-25 14:29:43 +0200

    aaa

--- step.sh
+++ step.sh
@@ -2,7 +2,7 @@
 set -e
 
 if [[ ! -z ${config_file_content} ]]; then
-  echo "${config_file_content}" > /etc/vpnc/default.conf
+  sudo echo "${config_file_content}" > /etc/vpnc/default.conf
 fi
 
 set -- ${commandline_options}
